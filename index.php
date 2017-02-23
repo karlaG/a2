3cb0344f273562ea0b0fdc33c968899e34148848
Karla Guardado <kmg023@g.harvard.edu>
2017-02-23 17:28:24 -0500

    Style fix

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 		<p>and my birthdate is</p>
 
 		<label for="month">*Month</label>
-		<select name="month" id="month" size="12" required>
+		<select name="month" id="month" size="3" required>
 			<option value="1">Jan</option>
 			<option value="2">Feb</option>
 			<option value="3">Mar</option>
